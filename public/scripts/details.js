017bbeba99ce4b8d98d87b1a21d33d88310e9bcf
Michelle Harrop <harrop.michelle@gmail.com>
2019-08-28 15:02:40 -0400

    created details page, including cards for details and a table to members, and javascript to dynamically create both of those

---
+++ public/scripts/details.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/*This ready function calls the insert table data function.
+* It also adds a register button that connects a user to the register page for this course
+*
+* @param - courseId - string = this is pulled from the course that the user selected on the course page
+*/
+$(function ()
+{
+    let urlParams = new URLSearchParams(location.search);
+    let teamId = urlParams.get("teamId");
+
+    /* this is the call to the server */
+    let teamInfo;
+    $.getJSON("api/teams/" + teamId, function (data)
+    {
+        teamInfo = data;
+        insertData(teamInfo);
+        insertMemberTable(teamInfo);
+
+        //$("#register").prop("href", "register.html?teamId=" + team.CourseId);
+    });
+
+    /* this links the back to details page button and sends it back to the details page for that course
+    $("#backToDetails").on("click", function ()
+    {
+        $("#backToDetails").prop("href", "search.html");
+    });
+
+    //this links to the edit course page associated with the specific course
+    $("#editClass").on("click", function ()
+    {
+        $("#editClass").prop("href", "editcourse.html?courseId=" + team.CourseId);
+    }); */
+});
+
+function insertData(team)
+{
+    $("#ManagerName").html("Manager's Name: " + team.ManagerName);
+    $("#ManagerPhone").html("Manager's Phone Number: " + team.ManagerPhone);
+    $("#ManagerEmail").html("Manager's Email: " + team.ManagerEmail)
+
+    $("#MaxTeamMembers").html("Maximum Number of Volunteers: " + team.MaxTeamMembers);
+    $("#MinMemberAge").html("Minimum Volunteer Age: " + team.MinMemberAge);
+    $("#MaxMemberAge").html("Maximum Volunteer Age: " + team.MaxMemberAge);
+    $("#TeamGender").html("Team Gender: " + team.TeamGender);
+}
+
+function insertMemberTable(team)
+{
+    if (team.Members.length == 0)
+    {
+        let noMembers = "<tr><td>No volunteers yet!</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>"
+        $("#memberTableBody").append(noMembers);
+    }
+    else
+    {
+        for (let i = 0; i < team.Members.length; i++)
+        {
+            let memberName = team.Members[i].MemberName;
+            let contact = team.Members[i].ContactName;
+            let email = team.Members[i].Email;
+            let phone = team.Members[i].Phone;
+            let age = team.Members[i].Age;
+            let gender = team.Members[i].Gender;
+
+            let htmlString = `
+                <tr>
+                    <td>${memberName}</td>
+                    <td>${contact}</td>
+                    <td>${email}</td>
+                    <td>${phone}</td>
+                    <td>${age}</td>
+                    <td>${gender}</td>
+                </tr>`;
+
+            $("#memberTableBody").append(htmlString);
+        }
+    }
+}
